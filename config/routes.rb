e87499a28b190eda18c1f48a97eb65d908f329a7
naomichi-h <naomichi.programming@gmail.com>
2021-06-14 13:24:14 +0900

    ユーザー一覧画面とユーザー詳細画面を追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   devise_for :users, controllers: {
     registrations: "users/registrations"
   }
+
+  resources :users, :only => [:index, :show]
+
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
