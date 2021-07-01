8a559d6fe069d5cfaf376fedd3de782cbc8cd2d4
naomichi-h <naomichi.programming@gmail.com>
2021-07-01 13:16:53 +0900

    fix:レビューを受けて修正を行いました
    不要なscssファイルを削除しました
    confirmableに関するファイルを削除しました
    omniauthに関するファイルを削除しました
    不要なコメントを削除しました

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     registrations: "users/registrations"
   }
 
-  resources :users, :only => [:index, :show]
+  resources :users, only: [:index, :show]
 
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
