9f7b8a54ff576cdf5fd2ac35a5736abcb5368fac
naomichi-h <naomichi.programming@gmail.com>
2021-08-26 17:53:31 +0900

    update: テストを追加しました
    ReportクラスとUserクラスのインスタンスメソッドに対するモデルテストを追加
    ログインから日報の新規作成、編集、削除に関するシステムテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+    report = Report.create!(user: me, title: 'ゴールデンカムイ読んだ感想', content: 'いろんな意味でヤバイ漫画')
+    assert report.editable?(me)
+    assert_not report.editable?(she)
+  end
+
+  test '#created_on' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    report = Report.create!(user: me, title: 'ゴールデンカムイ読んだ感想', content: 'いろんな意味でヤバイ漫画')
+    assert report.created_on
+    assert_not_equal report.created_on, report.created_at
+  end
 end
